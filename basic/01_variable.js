a84b8f19a7e6417f424db886e87793b195420fa9
shubham.singh <shubham.singh@coverself.com>
2024-12-30 14:50:26 +0530

    dataType Reason: Fixes:

---
+++ basic/01_variable.js
@@ -0,0 +1,16 @@
+const accountId = 1234567890
+let accountEmail = "shubham@gmail.com"
+var accountPass = "shubham"
+accountCity = "Ranchi"
+
+/*
+prefer not to use var
+*/
+console.log(accountId)
+console.log(accountEmail)
+console.log(accountPass)
+console.log(accountCity)
+
+// only use const and let
+
+console.table([accountId, accountEmail])
---
+++ basic/02_dataType.js
@@ -0,0 +1,21 @@
+/*
+
+number
+bigint
+String >  ""
+boolean > 
+null > standalone value
+undefined 
+symbol > unique
+
+//Object
+
+*/
+
+let name = " shubham"  // String
+let age = 18  // number
+let isLoggedIn = false // bolean 
+
+console.log(typeof(age))
+
+
